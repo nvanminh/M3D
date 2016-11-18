04963b0fd1b285798f5639945253db84e4fbbf16
Jackie.Nguyen <nvanminh@tma.com.vn>
2016-11-18 11:49:11 +0700

    Modify hellow workd store proc

--- dbo/Stored Procedures/p_Testing_HelloWorld.sql
+++ dbo/Stored Procedures/p_Testing_HelloWorld.sql
@@ -1,5 +1,10 @@
-﻿CREATE PROCEDURE [p_Testing_HelloWorld]
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[p_Testing_HelloWorld]
 AS
 BEGIN
-   SELECT 'Hello world!' AS Message;
+   SELECT 'Hello world! How are you?' AS Message;
 END
+GO
